1c1e2e33806dcf9ce31f73007ecdf382b8628221
haptelmanovartem <haptelmanovartem@gmail.com>
2021-12-03 00:34:05 +0200

    break camelCase and Rot13 encoding

---
+++ src/Break camelCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Break_camelCase
+{
+  internal static class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine(BreakCamelCase("testTestTestiong"));
+    }
+    
+    public static string BreakCamelCase(string str)
+    {
+      var sb = new StringBuilder();
+      
+      foreach (var charItem in str)
+      {
+        if (char.IsUpper(charItem))
+        {
+          sb.Append($" {charItem}");
+          continue;
+        }
+
+        sb.Append(charItem);
+      }
+
+      return sb.ToString();
+    }
+  }
+}
---
+++ src/Rot13/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace Rot13
+{
+  /// <summary>
+  /// https://www.codewars.com/kata/530e15517bc88ac656000716/train/csharp
+  /// </summary>
+  internal static class Program
+  {
+    private const int LatinUpperStartIndex = 65;
+    private const int LatinUpperEndIndex = 90;
+    private const int LatinLowerStartIndex = 97;
+    private const int LatinLowerEndIndex = 122;
+    private const int Rot13Index = 13;
+
+    private static void Main(string[] args)
+    {
+      Console.WriteLine(Rot13("test"));
+    }
+    
+    public static string Rot13(string message)
+    {
+      var sb = new StringBuilder();
+
+      foreach (var item in message)
+      {
+        if (item >= LatinUpperStartIndex && item <= LatinUpperEndIndex)
+        {
+          sb.Append(GetRot13Symbol(item, LatinUpperStartIndex, LatinUpperEndIndex));
+        }
+        else if (item >= LatinLowerStartIndex && item <= LatinLowerEndIndex)
+        {
+          sb.Append(GetRot13Symbol(item, LatinLowerStartIndex, LatinLowerEndIndex));
+        }
+        else
+          sb.Append(item);
+      }
+
+      return sb.ToString();
+    }
+
+    private static char GetRot13Symbol(
+      char index,
+      int start, 
+      int end)
+    {
+      var position = index;
+      var counter = 0;
+      
+      while (counter < Rot13Index)
+      {
+        if (++position > end)
+        {
+          position = (char)start;
+        }
+
+        counter++;
+      }
+
+      return position;
+    }
+  }
+}
