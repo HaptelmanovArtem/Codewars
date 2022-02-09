6b3e498175e739ed550899f0f4c3bf3f5157a9ec
haptelmanovartem <haptelmanovartem@gmail.com>
2022-02-09 18:47:14 +0200

    add katas

---
+++ src/Make a spiral/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Make_a_spiral
+{
+  internal class Program
+  {
+    private const int Empty = 0;
+    private const int Element = 1;
+
+    private static void Main(string[] args)
+    {
+      var spiral = Spiralize(5);
+
+      Print(spiral);
+    }
+
+    private static void Print(int[,] spiral)
+    {
+      for (var i = 0; i < 5; i++)
+      {
+        for (var j = 0; j < 5; j++)
+        {
+          Console.Write($" {spiral[i, j]} ");
+        }
+
+        Console.WriteLine();
+      }
+
+      Console.WriteLine();
+      Console.WriteLine();
+    }
+
+    public static int[,] Spiralize(int size)
+    {
+      if (size <= 0)
+        return null;
+
+      var currentDirection = Direction.Right;
+
+      var result = new int[size, size];
+
+      var i = 0;
+      var j = 0;
+
+      while (true)
+      {
+        if (!IsAnyPathAvailable(result, i, j, size))
+        {
+          if (CanSetElement(result, i, j, size, currentDirection))
+          {
+            result[i, j] = 1;
+          }
+          
+          break;
+        }
+
+        if (currentDirection == Direction.Right)
+        {
+          if (CanGoRight(result, i, j, size))
+          {
+            result[i, j] = 1;
+            j++;
+            continue;
+          }
+
+          currentDirection = Direction.Down;
+        }
+        else if (currentDirection == Direction.Down)
+        {
+          if (CanGoDown(result, i, j, size))
+          {
+            result[i, j] = 1;
+            i++;
+            continue;
+          }
+
+          currentDirection = Direction.Left;
+        }
+        else if (currentDirection == Direction.Left)
+        {
+          if (CanGoLeft(result, i, j))
+          {
+            result[i, j] = 1;
+            j--;
+            continue;
+          }
+
+          currentDirection = Direction.Top;
+        }
+        else if (currentDirection == Direction.Top)
+        {
+          if (CanGoTop(result, i, j))
+          {
+            result[i, j] = 1;
+            i--;
+            continue;
+          }
+
+
+          currentDirection = Direction.Right;
+        }
+      }
+
+      return result;  
+    }
+
+    private static bool CanSetElement(int[,] arr, int i, int j, int size, Direction direction)
+    {
+      var isTop = i - 1 < 0 || arr[i - 1, j] == 0;
+      var isDown = i + 1 >= size || arr[i + 1, j] == 0;
+      var isLeft = j - 1 < 0 || arr[i, j - 1] == 0;
+      var isRight = j + 1 >= size || arr[i, j + 1] == 0;
+
+      return direction switch
+      {
+        Direction.Left => isTop && isDown && isLeft,
+        Direction.Down => isDown && isLeft && isRight,
+        Direction.Right => isTop && isDown && isRight,
+        Direction.Top => isTop && isLeft && isRight,
+        _ => throw new ArgumentOutOfRangeException(nameof(direction))
+      };
+    }
+
+    private static bool IsAnyPathAvailable(int[,] arr, int i, int j, int size)
+    {
+      return
+        CanGoRight(arr, i, j, size) ||
+        CanGoDown(arr, i, j, size) ||
+        CanGoLeft(arr, i, j) ||
+        CanGoTop(arr, i, j);
+    }
+
+    private static bool CanGoRight(int[,] array, int currentIndexI, int currentIndexJ, int size)
+    {
+      var isNextEmpty = currentIndexJ + 1 < size &&
+                        array[currentIndexI, currentIndexJ + 1] == Empty;
+
+      var isNextNextHasElement = currentIndexJ + 2 <= size - 1 &&
+                                 array[currentIndexI, currentIndexJ + 2] != Empty;
+
+      return isNextEmpty && !isNextNextHasElement;
+    }
+
+    private static bool CanGoDown(int[,] array, int currentIndexI, int currentIndexJ, int size)
+    {
+      var isHasNextElement = currentIndexI + 1 < size &&
+                             array[currentIndexI + 1, currentIndexJ] == Empty;
+
+      var isNextNextHasElement = currentIndexI + 2 <= size - 1 &&
+                                 array[currentIndexI + 2, currentIndexJ] != Empty;
+
+      return isHasNextElement && !isNextNextHasElement;
+    }
+
+    private static bool CanGoLeft(int[,] array, int currentIndexI, int currentIndexJ)
+    {
+      var isHasNextElement = currentIndexJ - 1 >= 0 &&
+                             array[currentIndexI, currentIndexJ - 1] == Empty;
+
+      var isNextNextHasElement = currentIndexJ - 2 >= 0 &&
+                                 array[currentIndexI, currentIndexJ - 2] != Empty;
+
+      return isHasNextElement && !isNextNextHasElement;
+    }
+
+    private static bool CanGoTop(int[,] array, int currentIndexI, int currentIndexJ)
+    {
+      var isHasNextElement = currentIndexI - 1 >= 0 &&
+                             array[currentIndexI - 1, currentIndexJ] == Empty;
+
+      var isNextNextHasElement = currentIndexI - 2 >= 0 &&
+                                 array[currentIndexI - 2, currentIndexJ] != Empty;
+
+      return isHasNextElement && !isNextNextHasElement;
+    }
+
+    private enum Direction
+    {
+      Left = 1,
+      Down = 2,
+      Right = 3,
+      Top = 4,
+    }
+  }
+}
---
+++ src/Next bigger number with the same digits/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Next_bigger_number_with_the_same_digits
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      NextBiggerNumber(21581957621);
+    }
+
+    public static long NextBiggerNumber(long n)
+    {
+      var s = n.ToString().ToCharArray();
+
+      var pivotIndex = -1;
+
+      for (var i = s.Length - 1; i >= 0; i--)
+      {
+        if (i - 1 < 0)
+          return -1;
+
+        if (s[i] > s[i - 1])
+        {
+          pivotIndex = i - 1;
+          break;
+        }
+      }
+
+      if (pivotIndex == -1)
+      {
+        return -1;
+      }
+
+      var rightSideArray = new List<char>();
+
+      for (var i = s.Length - 1; i > pivotIndex; i--)
+      {
+        rightSideArray.Add(s[i]);
+      }
+
+      rightSideArray = rightSideArray.OrderBy(i => i).ToList();
+      var swapValueIndex = -1;
+
+      for (var j = 0; j < rightSideArray.Count; j++)
+      {
+        if (s[pivotIndex] < rightSideArray[j])
+        {
+          swapValueIndex = j;
+          break;
+        }
+      }
+
+      if (swapValueIndex == -1)
+        return -1;
+
+      var pivotTemp = s[pivotIndex];
+      s[pivotIndex] = rightSideArray[swapValueIndex];
+      rightSideArray[swapValueIndex] = pivotTemp;
+      
+      var result = new[] {s.ToList().Take(pivotIndex + 1), rightSideArray}
+        .SelectMany(i => i)
+        .ToList();
+
+      return long.Parse(result.ToArray());
+    }
+  }
+}
