993f168abb2c70771356957dca83fc82a3e36c59
a.haptelmanov <haptelmanovartem@gmail.com>
2024-05-10 00:27:13 +0300

    try to implement SerializeAndDeserializeBinaryTree, current proble is overflow of index field, possible solution use partition.

--- General/Program.cs
+++ General/Program.cs
@@ -5,25 +5,12 @@ Console.WriteLine("Run1");
 var serializer = new SerializeAndDeserializeBinaryTree();
 
 var root = new TreeNode(1);
-var leftLeaf1 = new TreeNode(2);
-var rightLeaf1 = new TreeNode(3);
-root.left = leftLeaf1;
-root.right = rightLeaf1;
-
-var leftLeftLeaf12 = new TreeNode(4);
-var leftRightLeaf12 = new TreeNode(5);
-leftLeaf1.left = leftLeftLeaf12;
-leftLeaf1.right = leftRightLeaf12;
-
-var leftLeftLeaf22 = new TreeNode(8);
-var leftRightLeaf22 = new TreeNode(64);
-leftLeftLeaf12.left = leftLeftLeaf22;
-leftLeftLeaf12.right = leftRightLeaf22;
-
-var rightLeftLeaf22 = new TreeNode(10);
-var rightRightLeaf22 = new TreeNode(100);
-leftRightLeaf12.left = rightLeftLeaf22;
-leftRightLeaf12.right = rightRightLeaf22;
+TreeNode current = root;
+foreach(var i in Enumerable.Range(2, 1000))
+{
+    current.right = new TreeNode(i);
+    current = current.right;
+}
 
 var serilizedData = serializer.Serialize(root);
 var deserializedData = serializer.Deserialize(serilizedData);
--- General/SerializeAndDeserializeBinaryTree.cs
+++ General/SerializeAndDeserializeBinaryTree.cs
@@ -8,100 +8,206 @@ namespace General
     /// </summary>
     public class SerializeAndDeserializeBinaryTree
     {
-        private static readonly char _delimiter = ',';
+        private static readonly char _delimiterBetweenNode = ',';
+        private static readonly char _delimiterBetweenParitition = ';';
         private static  readonly string _emptyNode = "null";
+        private static readonly int _numberOfNodesToMakePartition = 13; // 2^15
 
         public string Serialize(TreeNode root)
         {
+            int numberOfProcessedNodes = 0;
+            int currentPartition = 0;
+
             if (root == null)
                 return string.Empty;
 
-            var result = new StringBuilder();
-            result.Append($"{root.val}{_delimiter}");
-
-            GetNextLevelNodesValue(root, result);
-
-            return result.ToString().TrimEnd(_delimiter);
-
-            void GetNextLevelNodesValue(TreeNode node, StringBuilder sb)
+            var result = new List<List<(int? Value, long Pos)>>
             {
-                var leftNodeValue = node.left is null ? _emptyNode : node.left.val.ToString();
-                var rightNodeValue = node.right is null ? _emptyNode : node.right.val.ToString();
+                new List<(int? Value, long Pos)>()
+            };
 
-                sb.Append($"{leftNodeValue}{_delimiter}");
-                sb.Append($"{rightNodeValue}{_delimiter}");
+            Traverse(root, result, 0, ref numberOfProcessedNodes, ref currentPartition);
 
-                if (node.left != null)
+
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < result.Count; i++)
+            {
+                var partition = result[i];
+                sb.Append('[');
+                sb.AppendJoin(_delimiterBetweenNode, partition.OrderBy(i => i.Pos).Select(dto => dto.Value.HasValue ? dto.Value.Value.ToString() : _emptyNode));
+                
+                if (i + 1 == result.Count)
+                    sb.Append(']');
+                else
+                    sb.Append($"]{_delimiterBetweenParitition}");
+
+            }
+
+            return sb.ToString();
+
+            static void Traverse(TreeNode node, List<List<(int? Value, long Pos)>> result, long currentIndex, ref int numberOfProcessedNodes, ref int currentPartition)
+            {
+                numberOfProcessedNodes++;
+                if (numberOfProcessedNodes == _numberOfNodesToMakePartition) 
                 {
-                    GetNextLevelNodesValue(node.left, sb);
+                    currentPartition++;
+                    result.Add(new List<(int? Value, long Pos)>());
+                    currentIndex = 0;
+                    numberOfProcessedNodes = 0;
                 }
 
-                if (node.right != null)
+                result[currentPartition].Add((node?.val, currentIndex));
+
+                if (node is null)
+                    return;
+
+                var leftNodePos = (currentIndex * 2) + 1;
+                var rightNodePos = (currentIndex * 2) + 2;
+
+                Traverse(node.left, result, leftNodePos, ref numberOfProcessedNodes, ref currentPartition);
+                Traverse(node.right, result, rightNodePos, ref numberOfProcessedNodes, ref currentPartition);
+            }
+        }
+
+        private static int CalculatePartitionArrayNumber(long index, int currentPartitionArrayNumber)
+        {
+            if (index > int.MaxValue)
+            {
+                return currentPartitionArrayNumber++;
+            }
+
+            return currentPartitionArrayNumber;
+        }
+
+        // example 
+        public TreeNode Deserialize(string data)
+        {
+            int numberOfProcessedNodes = 0;
+            int currentPartition = 0;
+
+            if (string.IsNullOrWhiteSpace(data))
+                return null;
+
+            var partitions = data.Split(_delimiterBetweenParitition);
+
+            var source = partitions[0].Split(_delimiterBetweenParitition);
+
+            var root = new TreeNode(int.Parse(source[0]));
+
+            Traverse(root, partitions, 0, ref numberOfProcessedNodes, ref currentPartition);
+
+            return root;
+
+            static void Traverse(TreeNode node, string[] partitions, long currentIndex, string[] source, ref int numberOfProcessedNodes, ref int currentPartition)
+            {
+                numberOfProcessedNodes++;
+
+                if (numberOfProcessedNodes == _numberOfNodesToMakePartition)
                 {
-                    GetNextLevelNodesValue(node.right, sb);
+                    currentPartition++;
+                    currentIndex = 0;
+                    numberOfProcessedNodes = 0;
+                    source = partitions[currentPartition].Split(_delimiterBetweenNode);
+                }
+                    source = partitions;
+
+                if (node == null) 
+                    return;
+
+
+                var leftNodePos = (currentIndex * 2) + 1;
+                var rightNodePos = (currentIndex * 2) + 2;
+
+                if (leftNodePos < partitions.Length && partitions[leftNodePos] != null && int.TryParse(partitions[leftNodePos], out var val1))
+                {
+                    node.left = new TreeNode(val1);
+                    Traverse(node.left, partitions, leftNodePos);
+                }
+
+                if (rightNodePos < partitions.Length && partitions[rightNodePos] != null && int.TryParse(partitions[rightNodePos], out var val2))
+                {
+                    node.right = new TreeNode(val2);
+                    Traverse(node.right, partitions, rightNodePos);
                 }
             }
         }
 
+        /*
+         *         public string Serialize(TreeNode root)
+        {
+            if (root == null)
+                return string.Empty;
+
+            var result = new List<(int? Value, int Pos)>();
+
+            Traverse(root, result, 0);
+
+            var maxPosition = result.Max(x => x.Pos);
+
+            var serialized = new string[maxPosition + 1];
+
+            for (var i = 0; i < serialized.Length; i++)
+                serialized[i] = _emptyNode;
+
+            foreach (var item in result)
+            {
+                if (item.Value.HasValue)
+                    serialized[item.Pos] = item.Value.Value.ToString();
+            }
+
+            return string.Join(_delimiterBetweenNode, serialized);
+
+            static void Traverse(TreeNode node, List<(int? Value, int Pos)> result, int currentIndex)
+            {
+                result.Add((node?.val, currentIndex));
+
+                if (node is null)
+                    return;
+
+                var leftNodePos = (currentIndex * 2) + 1;
+                var rightNodePos = (currentIndex * 2) + 2;
+
+                Traverse(node.left, result, leftNodePos);
+                Traverse(node.right, result, rightNodePos);
+            }
+        }
+
+        // example 
         public TreeNode Deserialize(string data)
         {
             if (string.IsNullOrWhiteSpace(data))
                 return null;
 
-            TreeNode root = null;
-            var source = data.Split(_delimiter);
-            var currentNodeIndex = 0;
+            var source = data.Split(_delimiterBetweenNode);
+            var root = new TreeNode(int.Parse(source[0]));
 
-            if (source.Length == 0)
-                return null;
-
-            root = new TreeNode(int.Parse(source[currentNodeIndex]));
-            var leftNodeOffset = 1;
-            var rightNodeOffset = 2;
-
-            if (leftNodeOffset < source.Length)
-            {
-                if (source[leftNodeOffset] != _emptyNode)
-                {
-                    var nodeValue = int.Parse(source[leftNodeOffset]);
-                    root.left = new TreeNode(nodeValue);
-                    Traverse(root.left, source, currentIndex: leftNodeOffset, leftNodeOffset + 1, rightNodeOffset + 1);
-                }
-            }
-
-            Traverse(root, source, 0, leftNodeOffset, rightNodeOffset);
+            Traverse(root, source, 0);
 
             return root;
 
-            static void Traverse(TreeNode node, string[] source, int currentIndex, int leftNodeOffset, int rightNodeOffset)
+            static void Traverse(TreeNode node, string[] source, int currentIndex)
             {
                 if (node == null) 
                     return;
 
-                var lnOffset = currentIndex + leftNodeOffset;
-                if (lnOffset < source.Length) 
+                var leftNodePos = (currentIndex * 2) + 1;
+                var rightNodePos = (currentIndex * 2) + 2;
+
+                if (source[leftNodePos] != _emptyNode)
                 {
-                    if (source[lnOffset] != _emptyNode)
-                    {
-                        var nodeValue = int.Parse(source[lnOffset]);
-                        node.left = new TreeNode(nodeValue);
-                    }
+                    node.left = new TreeNode(int.Parse(source[leftNodePos]));
+                    Traverse(node.left, source, leftNodePos);
                 }
 
-                var rnOffset = currentIndex + rightNodeOffset;
-                if (rnOffset < source.Length)
+                if (source[rightNodePos] != _emptyNode)
                 {
-                    if (source[rnOffset] != _emptyNode)
-                    {
-                        var nodeValue = int.Parse(source[rnOffset]);
-                        node.right = new TreeNode(nodeValue);
-                    }
+                    node.right = new TreeNode(int.Parse(source[rightNodePos]));
+                    Traverse(node.right, source, rightNodePos);
                 }
-
-                Traverse(node.left, source, lnOffset, leftNodeOffset + 1, rightNodeOffset + 2);
-                Traverse(node.right, source, rnOffset, leftNodeOffset + 1, rightNodeOffset + 1);
             }
         }
+         */
     }
 
     public class TreeNode
