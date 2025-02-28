0b204771d0a2a8652fba0296aa07093b0f9fb3ee
a.haptelmanov <haptelmanovartem@gmail.com>
2025-02-28 19:30:46 +0100

    two sum

---
+++ General/TwoSum2Indexes.cs
@@ -0,0 +1,32 @@
+﻿namespace General
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+        /// </summary>
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var i = 0;
+            var y = numbers.Length - 1;
+
+            while (i < y)
+            {
+                var sum = numbers[i] + numbers[y];
+
+                if (target == sum)
+                {
+                    return new int[] { i + 1, y + 1 };
+                }
+
+                if (target < sum)
+                    y--;
+
+                if (target > sum)
+                    i++;
+            }
+
+            return new int[] { };
+        }
+    }
+}
